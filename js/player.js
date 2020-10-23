b4d63c84a24ac1bf7a41bdcc92e47385d092949a
Dan Rigall <danrigall@gmail.com>
2020-10-22 18:18:48 -0700

    refac: (moves hero card creation to player.js)

--- js/player.js
+++ js/player.js
@@ -13,3 +13,27 @@ let hero00  = new Player("Dutch", 'dutch', 19, 13, 9, 'Veteran Special Operator.
 let hero01 = new Player("Douglas Quaid", 'quaid', 15, 12, 12, 'Mild-mannered construction worker. Likes demure women. Not a fan of parties.');
 let hero02 = new Player("Harry Tasker", 'tasker', 18, 12, 10, 'Veteran Special Agent with an iron-clad secret identity. Sometimes has marital troubles. Not a fan of car salesmen.');
 let hero03 = new Player("John Matrix", 'matrix', 20, 15, 8, 'Commando by trade. Lumberjack by necessity. Father of the Year.');
+
+function setHeroes(wrapper) {
+    const heroes = [hero00, hero01, hero02, hero03]
+    let hero
+    for (hero of heroes) {
+        const heroCard = document.createElement('div')
+        heroCard.classList.add('heroWrapper')
+        heroCard.innerHTML = `
+            <a href="#" onclick="Main.startSettings('${hero.classType}')">
+                <div class="hero">
+                    <h3>${hero.classType}</h3>
+                    <img src="./img/${hero.NN}.png" alt="${hero.classType}">
+                    <p>${hero.DESC}</p>
+                </div>
+            </a>
+        `
+        wrapper.appendChild(heroCard)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const wrapper = document.getElementById('cardWrapper')
+    setHeroes(wrapper)
+})
